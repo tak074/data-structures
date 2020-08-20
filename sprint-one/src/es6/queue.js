e2dcbd5a111928b65099bc27df76ad50bd1e4491
unknown <tak@ucsd.com>
2020-08-20 16:39:45 -0700

    completed es6 section

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,30 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.index = 0;
+  }
+
+  enqueue(value) {
+    this[this.index + 1] = value;
+    this.index++;
+  }
+
+  dequeue() {
+    if (this.index !== 0) {
+      var dqVal = this[1];
+      delete this[1];
+      for (var key in this) {
+        this[key - 1] = this[key];
+      }
+      this.index--;
+      return dqVal;
+    } else {
+      return this;
+    }
+  }
+
+  size() {
+    return this.index;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.index = 0;
+  }
+
+  pop() {
+    if (this.index !== 0) {
+      var poppedElement = this[this.index];
+      delete this[this.index];
+      this.index --;
+      return poppedElement;
+    } else {
+      return this;
+    }
+  }
+
+  push(value) {
+    this[this.index + 1] = value;
+    this.index++;
+  }
+
+  size() {
+    return this.index;
   }
 
 }
