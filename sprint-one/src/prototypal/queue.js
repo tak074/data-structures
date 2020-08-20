20aee027cd8e6b00cf23145dca16323cb758f5a0
unknown <tak@ucsd.com>
2020-08-20 15:36:51 -0700

    prototypal section

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  //create instance object.create();
+  var someInstance = Object.create(queueMethods);
+  someInstance.index = 0;
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this[this.index + 1] = value;
+    this.index++;
+  },
+  dequeue: function() {
+    if (this.index !== 0) {
+      var dqVal = this[1];
+      delete this[1];
+      for (var key in this) {
+        this[key -1] = this[key];
+      }
+      this.index--;
+      return dqVal;
+    } else {
+      return this;
+    }
+  },
+  size: function () {
+    return this.index;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // create using Object.create
+  var someInstance = Object.create(stackMethods);
+  // create index property
+  someInstance.index = 0;
+  // return new object
+  return someInstance;
 };
 
-var stackMethods = {};
+// Object.create();
+
+var stackMethods = {
+  pop: function() {
+    if (this.index === 0) {
+      return this;
+    } else {
+      var poppedElement = this[this.index];
+      delete this[this.index];
+      this.index--;
+      return poppedElement;
+    }
+  },
+  push: function(value) {
+    this[this.index + 1] = value;
+    this.index++;
+  },
+
+  size: function() {
+    return this.index;
+  }
+};
+
 
 
