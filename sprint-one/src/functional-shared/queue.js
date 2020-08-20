80ad4fcb6640436e61fb14fc59c8f0dc0886660f
unknown <tak@ucsd.com>
2020-08-20 13:23:36 -0700

    finished functional-shared portion

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,43 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // create an empty obj
+  var someInstance = {};
+
+  someInstance.storage = {};
+  // property of someInstane
+  someInstance['lastIndex'] = 0;
+
+  // extend to connect someInstance to queueMethods
+  _.extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  // enqueue method
+  enqueue: function (value) {
+    // someInstance[lastIndex + 1] = value
+    this.storage[this.lastIndex + 1] = value;
+    // lastIndex++
+    this.lastIndex++;
+  },
+  dequeue: function () {
+    if (this.lastIndex !== 0) {
+      var dqVal = this.storage['1'];
+      delete this.storage['1'];
+      this.lastIndex--;
+      for (var key in this.storage) {
+        this.storage[key - 1] = this.storage[key];
+      }
+      return dqVal;
+    } else {
+      return this.storage;
+    }
+  },
+  size: function () {
+    return this.lastIndex;
+  }
 
+};
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -3,18 +3,37 @@ var Stack = function() {
   // but try not not reference your old code in writing the new style.
 
   // create someInstance obj
+  var someInstance = {};
   // create size prop equal 0
+  someInstance.lastIndex = 0;
   // pass methods to instance by using extend
+  _.extend(someInstance, stackMethods);
 
-
+  return someInstance;
 };
 
-// for in loo
 
 var stackMethods = {
   // create pop method using this keyword
+  pop: function() {
+    if (this.lastIndex !== 0) {
+      var poppedItem = this[this.lastIndex];
+      delete this[this.lastIndex];
+      this.lastIndex--;
+      return poppedItem;
+    } else {
+      return this;
+    }
+  },
   // create push methods using this keywords
+  push: function(value) {
+    this[this.lastIndex + 1] = value;
+    this.lastIndex++;
+  },
   // create size method using this
+  size: function () {
+    return this.lastIndex;
+  }
 };
 
 
