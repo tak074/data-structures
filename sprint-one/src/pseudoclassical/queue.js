4beb6a62bcf6c8d304f7cb89d01c48008c60ecf9
unknown <tak@ucsd.com>
2020-08-20 16:19:31 -0700

    completed pseudoclassical section

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.lastIndex = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  // add the value to the end
+  this[this.lastIndex + 1] = value;
+  // increment
+  this.lastIndex++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.lastIndex !== 0) {
+    var dqVal = this[1];
+    delete this[1];
+    for (var key in this) {
+      this[key - 1] = this[key];
+    }
+    this.lastIndex--;
+    return dqVal;
+  } else {
+    return this;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.lastIndex;
+};
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.lastIndex = 0;
 };
 
 
+Stack.prototype.pop = function () {
+  if (this.lastIndex !== 0) {
+    var poppedElement = this[this.lastIndex];
+    delete this[this.lastIndex];
+    this.lastIndex--;
+    return poppedElement;
+  } else {
+    return this;
+  }
+};
+
+Stack.prototype.push = function (value) {
+  this[this.lastIndex + 1] = value;
+  this.lastIndex++;
+};
+
+Stack.prototype.size = function() {
+  return this.lastIndex;
+};
