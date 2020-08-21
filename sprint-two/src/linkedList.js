3eab430e2f711648c204d7553d9251097016fd7f
unknown <tak@ucsd.com>
2020-08-21 15:57:50 -0700

    finished linkedLIst and tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,51 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //create a newNode through Node
+    var newNode = new Node(value);
+
+    // if empty
+    if (!list.tail) {
+      // this.head = newNode;
+      this.head = newNode;
+      // this.tail = newNode;
+      this.tail = newNode;
+
+    } else {
+      //if not empty
+      var currentTail = this.tail;
+      currentTail.next = newNode;
+      this.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    if (list.head !== null) {
+      //currentHead = this.head;
+      var currentHead = this.head;
+      //newHead = currentHead.next;
+      var newHead = currentHead.next;
+      // delete currentHead;
+      this.head = newHead;
+      return currentHead.value;
+    } else {
+      return this;
+    }
   };
 
   list.contains = function(target) {
+    // create truthyVal = false;
+    var truthyVal = false;
+    //currentNode = this.head;
+    var currentNode = this.head;
+    while (!truthyVal && currentNode !== null) {
+      if (currentNode.value === target) {
+        truthyVal = true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+    return truthyVal;
   };
 
   return list;
@@ -26,4 +65,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addToTail has a O(1)
+ removeHead has a O(1)
+ contains has a O(n)
  */
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = []; // fix me
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,13 +13,28 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Tree(value);
+  var mother = this;
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  var truthyVal = false;
+  var currentNode = this;
+
+  var innerFunc = function(currentNode) {
+    if (currentNode.value === target) {
+      truthyVal = true;
+    } else {
+      if (currentNode.children) {
+        for (var i = 0; i < currentNode.children.length; i++) {
+          innerFunc(currentNode.children[i]);
+        }
+      }
+    }
+  };
+
+  innerFunc(currentNode);
+  return truthyVal;
 };
 
-
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
