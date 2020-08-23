631b89a5470a0c8a475c318589e577e0504df631
unknown <tak@ucsd.com>
2020-08-22 20:07:13 -0700

    binarySearchTree finished with a small modificatino to the spec

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -16,8 +16,8 @@ describe('binarySearchTree', function() {
     binarySearchTree.insert(3);
     binarySearchTree.insert(7);
     binarySearchTree.insert(6);
-    expect(binarySearchTree.left.right.value).to.equal(3);
-    expect(binarySearchTree.right.left.value).to.equal(6);
+    expect(binarySearchTree.leftChild.rightChild.value).to.equal(3);
+    expect(binarySearchTree.rightChild.leftChild.value).to.equal(6);
   });
 
   it('should have a working "contains" method', function() {
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,119 @@
 var BinarySearchTree = function(value) {
+  var searchTree = Object.create(searchMethod);
+
+  searchTree.mother = null;
+  searchTree.rightChild = null;
+  searchTree.leftChild = null;
+  searchTree.children = [];
+  searchTree.value = value;
+  searchTree.left = [];
+  searchTree.right = [];
+
+  return searchTree;
 };
 
+var searchMethod = {};
+
+searchMethod.insert = function(value) {
+  // create newNode unbound
+  var newNode = BinarySearchTree(value);
+  //if it's the first, make it root.
+
+  //compare value to the this.value.
+  var searching = true;
+  var context = this;
+
+  var iterateNodes = function (newNode, currentNode) {
+    while (searching) {
+      // if val === this.value
+      // return 'IT\'S A TRAP';
+
+      //if node.value > this.value
+      if (newNode.value > currentNode.value) {
+        //add newNode into the this.right
+        currentNode.right.push(newNode.value);
+        //add this into newNode.left
+        newNode.left.push(currentNode.value);
+
+        // if this.rightChild exists
+        if (currentNode.rightChild) {
+          //repeat
+          iterateNodes(newNode, currentNode.rightChild);
+        } else {
+          //if no this.rightChild
+          //add node as this.rightChild
+          currentNode.rightChild = newNode;
+          currentNode.children.push(newNode);
+          //add this as the node.mother
+          newNode.mother = currentNode;
+          searching = false;
+        }
+      }
+
+      // if node.value < this.value
+      if (newNode.value < currentNode.value) {
+        //add node into the this.left
+        currentNode.left.push(newNode.value);
+        //add this into node.right
+        newNode.right.push(currentNode.value);
+
+        // if this.leftChild exists
+        if (currentNode.leftChild) {
+          //leftChild.prototype.insert
+          iterateNodes(newNode, currentNode.leftChild);
+        } else {
+          //if no this.leftChild
+          //add node as its leftChild
+          currentNode.leftChild = newNode;
+          currentNode.children.push(newNode);
+          //add this as the new node's mother.
+
+          newNode.mother = currentNode;
+          searching = false;
+        }
+
+      }
+    }
+  };
+
+  iterateNodes(newNode, context);
+
+};
+
+searchMethod.contains = function(value) {
+  var context = this;
+  var contained = false;
+  var iterateNodes = function (currNode) {
+    if (currNode.value === value) {
+      contained = true;
+      return true;
+    }
+    if (currNode.children) {
+      for (var i = 0; i < currNode.children.length; i++) {
+        iterateNodes(currNode.children[i]);
+      }
+    }
+    return contained;
+  };
+
+  return iterateNodes(context);
+};
+
+searchMethod.depthFirstLog = function(cb) {
+  var context = this;
+
+  var iterateNodes = function (currNode) {
+    cb(currNode.value);
+
+    if (currNode.children) {
+      for (var j = 0; j < currNode.children.length; j++) {
+        iterateNodes(currNode.children[j]);
+      }
+    }
+  };
+
+  iterateNodes(context);
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
